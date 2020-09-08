0cefd77c5c54148a882bc706c2ebfe4dde67840d
Elizabeth Guerra <ecguerra@outlook.com>
2020-09-08 18:57:02 -0400

    Year 1 - added h1 using javascript

---
+++ app.js
@@ -0,0 +1,10 @@
+const containter = document.querySelector('#container')
+// console.log(container)
+
+const h1 = document.createElement('h1')
+h1.innerText = 'Welcome to Hogwarts'
+
+containter.appendChild(h1)
+
+// console.log(h1)
+
