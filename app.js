49c20f001caf77b62746c3bd3528775b0f233be4
Elizabeth Guerra <ecguerra@outlook.com>
2020-09-08 19:51:22 -0400

    Year 4 - added class schedule

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+//Year 1 - Setup
+
 const container = document.querySelector('#container')
 // console.log(container)
 
@@ -6,6 +8,8 @@ h1.innerText = 'Welcome to Hogwarts'
 
 container.appendChild(h1)
 // console.log(h1)
+
+// Year 2 - My Info
 
 const me = document.createElement('h2')
 me.innerText = 'Elizabeth Guerra'
@@ -24,8 +28,12 @@ const wand = document.createElement('h4')
 wand.innerText = 'Hornbeam and Dragon Heartstring'
 container.appendChild(wand)
 
+//Year 3 - Trunk & Contents
+
 const trunk = document.createElement('ul')
 trunk.setAttribute('storage','trunk')
+
+//Make into function
 
 const butterbeer = document.createElement('li')
 butterbeer.innerText ='butterbeer'
@@ -56,3 +64,67 @@ bbBeans.innerText = 'Bertie Bott\'s Every Flavor Beans'
 trunk.appendChild(bbBeans)
 
 container.appendChild(trunk)
+
+//Year 4 - Class Schedule
+const semester = document.createElement('h5')
+semester.innerText = 'Fall 2020'
+container.appendChild(semester)
+
+const schedule = document.createElement('table')
+container.appendChild(schedule)
+
+const header = document.createElement('thead')
+schedule.appendChild(header)
+
+const day = document.createElement('th')
+day.innerText = 'Day'
+header.appendChild(day)
+
+const classes = document.createElement('th')
+classes.innerText = 'Classes'
+header.appendChild(classes)
+
+const monRow = document.createElement('tr')
+schedule.appendChild(monRow)
+const mon = document.createElement('td')
+mon.innerText = 'Monday'
+monRow.appendChild(mon)
+const monClass = document.createElement('td')
+monClass.innerText = 'Transfiguration, Potions'
+monRow.appendChild(monClass)
+
+const tueRow = document.createElement('tr')
+schedule.appendChild(tueRow)
+const tue = document.createElement('td')
+tue.innerText = 'Tuesday'
+tueRow.appendChild(tue)
+const tueClass = document.createElement('td')
+tueClass.innerText = 'Defense Against the Dark Arts, Charms'
+tueRow.appendChild(tueClass)
+
+const wedRow = document.createElement('tr')
+schedule.appendChild(wedRow)
+const wed = document.createElement('td')
+wed.innerText = 'Wednesday'
+wedRow.appendChild(wed)
+const wedClass = document.createElement('td')
+wedClass.innerText = '<free period>, Arithmancy'
+wedRow.appendChild(wedClass)
+
+const thuRow = document.createElement('tr')
+schedule.appendChild(thuRow)
+const thu = document.createElement('td')
+thu.innerText = 'Thursday'
+thuRow.appendChild(thu)
+const thuClass = document.createElement('td')
+thuClass.innerText = 'Care of Magical Creatues, Ancient Runes'
+thuRow.appendChild(thuClass)
+
+const friRow = document.createElement('tr')
+schedule.appendChild(friRow)
+const fri = document.createElement('td')
+fri.innerText = 'Friday'
+friRow.appendChild(fri)
+const friClass = document.createElement('td')
+friClass.innerText = 'Herbology, <free period>'
+friRow.appendChild(friClass)
