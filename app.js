d97b01545e912eb8a2a9d93e75de7be5a5fa10bf
Elizabeth Guerra <ecguerra@outlook.com>
2020-09-08 19:20:37 -0400

    Added list of trunk items

--- app.js
+++ app.js
@@ -1,25 +1,58 @@
-const containter = document.querySelector('#container')
+const container = document.querySelector('#container')
 // console.log(container)
 
 const h1 = document.createElement('h1')
 h1.innerText = 'Welcome to Hogwarts'
 
-containter.appendChild(h1)
+container.appendChild(h1)
 // console.log(h1)
 
 const me = document.createElement('h2')
 me.innerText = 'Elizabeth Guerra'
-containter.appendChild(me)
+container.appendChild(me)
 
 const house = document.createElement('h3')
 house.innerText = 'Hufflepuff'
-containter.appendChild(house)
+container.appendChild(house)
 
 const pet = document.createElement('h4')
 pet.innerText = 'Tali'
 pet.classList.add('cat')
-containter.appendChild(pet)
+container.appendChild(pet)
 
 const wand = document.createElement('h4')
 wand.innerText = 'Hornbeam and Dragon Heartstring'
-containter.appendChild(wand)
+container.appendChild(wand)
+
+const trunk = document.createElement('ul')
+trunk.setAttribute('storage','trunk')
+
+const butterbeer = document.createElement('li')
+butterbeer.innerText ='butterbeer'
+trunk.appendChild(butterbeer)
+
+const invCloak = document.createElement('li')
+invCloak.classList.add('secret')
+invCloak.innerText = 'invisibility cloak'
+trunk.appendChild(invCloak)
+
+const magicMap = document.createElement('li')
+magicMap.classList.add('secret')
+magicMap.innerText = 'magic map'
+trunk.appendChild(magicMap)
+
+const timeTurner = document.createElement('li')
+timeTurner.classList.add('secret')
+timeTurner.innerText = 'time turner'
+trunk.appendChild(timeTurner)
+
+const leash = document.createElement('li')
+leash.classList.add('cat')
+leash.innerText = 'leash'
+trunk.appendChild(leash)
+
+const bbBeans = document.createElement('li')
+bbBeans.innerText = 'Bertie Bott\'s Every Flavor Beans'
+trunk.appendChild(bbBeans)
+
+container.appendChild(trunk)
